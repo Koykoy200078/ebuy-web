ff2e3a07b65f22d70ed950c8c4f91e8db9de1b6f
Koykoy200078 <franc200078@gmail.com>
2023-03-01 20:40:11 +0800

    update: products and categories

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\api\CategoryController;
+use App\Http\Controllers\api\ProductController;
 use App\Http\Controllers\api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -21,5 +22,8 @@ Route::group([
 
         // Product
         Route::get('/products', [ProductController::class, 'index']);
+
+        // Category
+        Route::get('/categories', [CategoryController::class, 'index']);
     });
 });

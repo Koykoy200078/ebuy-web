4febc8ddb27142c881fd78e8773f9fc1b776e1ff
Koykoy200078 <franc200078@gmail.com>
2023-04-28 11:45:44 +0800

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -17,28 +18,28 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
-    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-    {{-- <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
-    {{-- <link  href={{asset('assets/css/main.css')}} rel="stylesheet"> --}}
+    <link href="{{ asset('assets/css/font-awesome.min.css') }}" rel="stylesheet">
+    <!-- Scripts -->
 
-    <link  href={{asset('assets/css/bootstrap.min.css')}} rel="stylesheet">
-    <link  href={{asset('assets/css/custom.css')}} rel="stylesheet">
+    <!-- <link href="{{asset('assets/css/main.css')}}" rel="stylesheet"> -->
+
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/custom.css') }}" rel="stylesheet">
     <!-- CSS -->
 
-    <link  href={{asset('assets/css/owl.carousel.min.css')}} rel="stylesheet">
-    <link  href={{asset('assets/css/owl.theme.default.min.css')}} rel="stylesheet">
-    {{-- Owl Carousal --}}
+    <link href="{{ asset('assets/css/owl.carousel.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/owl.theme.default.min.css') }}" rel="stylesheet">
+    <!-- {{-- Owl Carousal --}} -->
 
-    <link  href={{asset('assets/exzoom/jquery.exzoom.css')}} rel="stylesheet">
-    {{-- EXZOOM Product Image --}}
+    <link href="{{ asset('assets/exzoom/jquery.exzoom.css') }}" rel="stylesheet">
 
-
-    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css"/>
+    <!-- {{-- EXZOOM Product Image --}} -->
+    <link rel="stylesheet" href="{{ asset('assets/css/alertify.min.css') }}" />
     <!-- Default theme -->
-    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/default.min.css"/>
+    <link rel="stylesheet" href="{{ asset('assets/css/default.min.css') }}" />
     @livewireStyles
 </head>
+
 <body>
     <div id="app">
 
@@ -55,29 +56,28 @@
 
     </div>
 
-    <script src="{{asset('assets/js/jquery-3.6.3.min.js')}}"></script>
-    <script src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{ asset('assets/js/jquery-3.6.3.min.js') }}">
+    </script>
+    <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
 
-    <script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
+    <script src="{{ asset('assets/js/alertify.min.js') }}"></script>
     <script>
-            window.addEventListener('message', event => {
-                if(event.detail){
-                    alertify.set('notifier','position', 'top-right');
-                    alertify.notify(event.detail.text, event.detail.type);
-                }
-
-            });
-
+        window.addEventListener('message', event => {
+            if (event.detail) {
+                alertify.set('notifier', 'position', 'top-right');
+                alertify.notify(event.detail.text, event.detail.type);
+            }
+        });
     </script>
 
-    <script src="{{asset('assets/js/owl.carousel.min.js')}}"></script>
+    <script src="{{ asset('assets/js/owl.carousel.min.js') }}"></script>
 
-    <script src="{{asset('assets/exzoom/jquery.exzoom.js')}}"></script>
+    <script src="{{ asset('assets/exzoom/jquery.exzoom.js') }}"></script>
 
-    <script src="{{ asset('admin/js/dashboard.js')}}"></script>
-    <script src="{{ asset('admin/js/data-table.js')}}"></script>
-    <script src="{{ asset('admin/js/jquery.dataTables.js')}}"></script>
-    <script src="{{ asset('admin/js/dataTables.bootstrap4.js')}}"></script>
+    <script src="{{ asset('admin/js/dashboard.js') }}"></script>
+    <script src="{{ asset('admin/js/data-table.js') }}"></script>
+    <script src="{{ asset('admin/js/jquery.dataTables.js') }}"></script>
+    <script src="{{ asset('admin/js/dataTables.bootstrap4.js') }}"></script>
     <!-- End custom js for this page-->
 
     @yield('scripts2')
@@ -88,4 +88,5 @@
     @stack('scripts')
 
 </body>
+
 </html>

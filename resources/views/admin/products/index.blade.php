3344d4ad0ca73f229ce81bb6d0146f4767b6111b
Your Name <fabillaralexander@gmail.com>
2023-05-09 23:32:59 +0800

    part2

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -11,7 +11,7 @@
         <div class="card">
             <div class="card-header">
                 <h3>Products
-                    <a href="{{ url('admin/products/create') }}" class="btn btn-primary btn-sm text-white float-end">
+                    <a href="{{ url('admin/products/create3') }}" class="btn btn-primary btn-sm text-white float-end">
                         Add Products
                     </a>
                 </h3>
